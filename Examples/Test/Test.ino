0b4b649e7b9a6d63e5d5ead667eb3f4bf4b079c6
ProjectoOfficial <miniprojectsofficial@gmail.com>
2021-10-20 01:46:54 +0200

    update example

--- Examples/Test/Test.ino
+++ Examples/Test/Test.ino
@@ -1,31 +1,64 @@
+/* This is a demo of OSCUP protocol, in this example you have to flash this
+ *  firmware on the ESP32 and then run the python code.
+ *  
+ *  This software is provided AS IS, no Warranties!
+ *  
+ *  Copyright © 2021 Projecto - Dott. Daniel Rossi, Dott. Riccardo Salami
+ *  License GPL-V3
+ */
+
 #include <stdlib.h>
 #include "oscup.h"
 
 uint8_t id = 0x5D;
 
+//constructor takes device's ID and baudrate
 Oscup oscup = Oscup(id, 115200);
 
 void setup() {
+  //it is mandatory to call begin() for starting UART
   oscup.begin();
 }
 
 void loop() {
   for (int i = 0; i < 10; i++) {
+    //this function gets the counter of TIM (returns a uint64_t)
     uint64_t tim = oscup.get_timer();
+    
+    //convert uint64_t to byte array
     char *arr = uint64_toBytes(tim);
-    uint8_t errore = oscup.write(0xF1, sizeof(uint64_t), arr);
+
+    //write the packet, parameters: command(uint8_t), length of the payload(uint8_t), payload(char *)
+    uint8_t error = oscup.write((uint8_t)TxCommands::SHARE, sizeof(uint64_t), arr);
+    
     delay(1);
+
+    //remember always to free dangling pointers!
     free(arr);
   }
   delay(1000);
+
+  //this function returns the APB clock frequency (set inside ESP32 libraries)
   uint64_t tim = oscup.get_APB_clk();
+  
+  //convert uint64_t to byte array
   char *arr2 = uint64_toBytes(tim);
-  uint8_t errore = oscup.write(0xF1, sizeof(uint64_t), arr2);
+  
+  //write the packet, parameters: command(uint8_t), length of the payload(uint8_t), payload(char *)
+  uint8_t errore = oscup.write((uint8_t)TxCommands::SHARE, sizeof(uint64_t), arr2);
+ 
+  //remember always to free dangling pointers!
   free(arr2);
   delay(5000);
 }
 
 char *uint64_toBytes(uint64_t number) {
+  /*@brief converts a uint64_t into a char * pointer
+   * 
+   * @param number uint64_t you want to convert
+   * 
+   * @return the pointer containing the new array of chars
+  */
   size_t dim = sizeof(uint64_t);
   char *buff = (char *)calloc(dim, sizeof(char));
   for (int i = 0 ; i < dim; i++) {
